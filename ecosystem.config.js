fdf1f12e60dde21d16d397d83cbd5e71c5acac76
makegoralex <makegoralex@yandex.ru>
2026-01-27 14:15:20 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "staff-site",
       cwd: __dirname,
-      script: "site/server.js",
+      script: "server.js",
       env: {
         NODE_ENV: "production",
         PORT: 4101
