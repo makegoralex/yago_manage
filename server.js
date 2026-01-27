13173198820e1f486778bb26353fb751a593e7e1
makegoralex <makegoralex@yandex.ru>
2026-01-27 15:44:04 +0300

    Fix bot login flow and preserve data

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/health") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    return res.end(JSON.stringify({ ok: true }));
-  }
-  res.writeHead(200);
-  res.end("staff alivev2");
-});
-
-const PORT = process.env.PORT || 4101;
-server.listen(PORT, () => console.log("staff running on", PORT));
+require("./site/server");
